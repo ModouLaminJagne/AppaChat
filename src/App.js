c0ecb9c1ca36111bdf8f32163f2522708d8a23ad
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-03-03 15:00:07 +0000

    Updating...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
-function App() {
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
   return (
-    <div className="App">
-      
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="f2fa0e5d-5ced-4cc4-be25-4fe0c5decfbf"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
-}
-
+};
 export default App;
